9072b09428b6aaa691af5d7364eb9b838a541240
M47CH4N <m47ch4n@gmail.com>
2020-03-07 17:40:06 +0900

    Add Authentication

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import useUser from '../hooks/use_user';
 
 export default () => {
-  return <div>Hello</div>;
+  const { user, load } = useUser();
+  return (
+    <div>
+      <p>ユーザーロード: {load ? '中' : '済'}</p>
+      <p>{user ? `こんにちは${user.displayName}さん` : 'サインインしてないよ'}</p>
+    </div>
+  );
 };
